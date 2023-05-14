9f5878ec5238d5c9727782507ce255b1e8c79c12
hvngover <hvngover17@gmail.com>
2023-05-15 02:16:18 +0600

    student

--- src/Student.java
+++ src/Student.java
@@ -3,7 +3,14 @@ class Student {
     private int age;
 
     public Student(String name, int age) {
-
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name can not be empty");
+        }
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Age between 0 and 120");
+        }
+        this.name = name;
+        this.age = age;
     }
 
 }
