d4dc76969e7a2da5dd7c411f42f04434c5af4d6a
hvngover <hvngover17@gmail.com>
2023-05-15 02:00:13 +0600

    hash

---
+++ src/MyHashTable.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+class MyHashTable<K, V> {
+    private List<LinkedList<Entry<K, V>>> buckets;
+    private int size;
+
+    public MyHashTable() {
+        this(25);
+    }
+
+    public MyHashTable(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be a positive value");
+        }
+        this.size = 0;
+        this.buckets = new ArrayList<>(capacity);
+        for (int i = 0; i < capacity; i++) {
+            buckets.add(new LinkedList<>());
+        }
+    }
+}
