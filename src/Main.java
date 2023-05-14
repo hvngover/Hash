109805820f2b57acc0a0799fa34fb57e1216ae2c
hvngover <hvngover17@gmail.com>
2023-05-15 01:55:33 +0600

    main

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,17 @@ public class Main {
             Student value = new Student("Major" + i, rand.nextInt(4) + 1);
             table.put(key, value);
         }
+        for (int i = 0; i < table.getCapacity(); i++) {
+            int bucketSize = table.getBucketSize(i);
+            System.out.println("Bucket " + i + " size: " + bucketSize);
+        }
+
+
+        table.clear();
+        for (int i = 0; i < 10000; i++) {
+            MyTests key = new MyTests(rand.nextInt(1000), "Name" + i);
+            Student value = new Student("Major" + i, rand.nextInt(4) + 1);
+            table.put(key, value);
+        }
     }
 }
