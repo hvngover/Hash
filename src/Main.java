01b19de2bae180992036c0931c959eb3da245066
hvngover <hvngover17@gmail.com>
2023-05-15 01:54:41 +0600

    main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        // Create new hash table with MyTestingClass keys and Student values
         MyHashTable<MyTests, Student> table = new MyHashTable<>();
 
-
+        Random rand = new Random();
+        for (int i = 0; i < 10000; i++) {
+            MyTests key = new MyTests(rand.nextInt(1000), "Name" + i);
+            Student value = new Student("Major" + i, rand.nextInt(4) + 1);
+            table.put(key, value);
+        }
     }
 }
