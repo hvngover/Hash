ad0b57423b621e321da1377be75589d816e9a52d
hvngover <hvngover17@gmail.com>
2023-05-15 02:18:03 +0600

    hash

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -2,13 +2,12 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-
 class MyHashTable<K, V> {
     private List<LinkedList<Entry<K, V>>> buckets; // List of linked lists to store entries
     private int size; // Number of elements in the hash table
 
     public MyHashTable() {
-        this(35); // Default capacity is 35
+        this(25); // Default capacity is 25
     }
 
     public MyHashTable(int capacity) {
@@ -16,7 +15,7 @@ class MyHashTable<K, V> {
             throw new IllegalArgumentException("Capacity must be a positive value");
         }
         this.size = 0;
-        this.buckets = new ArrayList<>(capacity);// Create a new ArrayList with specified capacity
+        this.buckets = new ArrayList<>(capacity); // Create a new ArrayList with specified capacity
         for (int i = 0; i < capacity; i++) {
             buckets.add(new LinkedList<>()); // Initialize each bucket with an empty linked list
         }
@@ -46,59 +45,60 @@ class MyHashTable<K, V> {
 
         bucket.add(new Entry<>(key, value)); // Add a new entry to the bucket
         size++;
+    }
 
-        public V get (K key){
-            int bucketIndex = getBucketIndex(key);
-            LinkedList<Entry<K, V>> bucket = buckets.get(bucketIndex);
-            for (Entry<K, V> entry : bucket) {
-                if (entry.key.equals(key)) {
-                    return entry.value; // Return the value associated with the key
-                }
+    public V get(K key) {
+        int bucketIndex = getBucketIndex(key);
+        LinkedList<Entry<K, V>> bucket = buckets.get(bucketIndex);
+        for (Entry<K, V> entry : bucket) {
+            if (entry.key.equals(key)) {
+                return entry.value; // Return the value associated with the key
             }
-            return null; // Key not found
         }
+        return null; // Key not found
+    }
 
-        public V remove (K key){
-            int bucketIndex = getBucketIndex(key);
-            LinkedList<Entry<K, V>> bucket = buckets.get(bucketIndex);
-            for (Entry<K, V> entry : bucket) {
-                if (entry.key.equals(key)) {
-                    bucket.remove(entry); // Remove the entry from the bucket
-                    size--;
-                    return entry.value; // Return the value of the removed entry
-                }
+    public V remove(K key) {
+        int bucketIndex = getBucketIndex(key);
+        LinkedList<Entry<K, V>> bucket = buckets.get(bucketIndex);
+        for (Entry<K, V> entry : bucket) {
+            if (entry.key.equals(key)) {
+                bucket.remove(entry); // Remove the entry from the bucket
+                size--;
+                return entry.value; // Return the value of the removed entry
             }
-            return null; // Key not found
         }
+        return null; // Key not found
+    }
 
-        public int getBucketSize ( int index){
-            if (index < 0 || index >= buckets.size()) {
-                throw new IllegalArgumentException("Invalid bucket index");
-            }
-            LinkedList<Entry<K, V>> bucket = buckets.get(index);
-            return bucket.size(); // Return the size of the specified bucket
+    public int getBucketSize(int index) {
+        if (index < 0 || index >= buckets.size()) {
+            throw new IllegalArgumentException("Invalid bucket index");
         }
+        LinkedList<Entry<K, V>> bucket = buckets.get(index);
+        return bucket.size(); // Return the size of the specified bucket
+    }
 
-        public void clear () {
-            for (LinkedList<Entry<K, V>> bucket : buckets) {
-                bucket.clear(); // Clear all the buckets
-            }
-            size = 0; // Reset the size to zero
+    public void clear() {
+        for (LinkedList<Entry<K, V>> bucket : buckets) {
+            bucket.clear(); // Clear all the buckets
         }
-        private int getBucketIndex (K key){
-            int hashCode = key.hashCode();
-            int bucketIndex = hashCode % buckets.size(); // Compute the bucket index using the hashCode
-            return Math.abs(bucketIndex); // Ensure a positive bucket index
-        }
+        size = 0; // Reset the size to zero
+    }
 
-        private static class Entry<K, V> {
-            private K key;
-            private V value;
+    private int getBucketIndex(K key) {
+        int hashCode = key.hashCode();
+        int bucketIndex = hashCode % buckets.size(); // Compute the bucket index using the hashCode
+        return Math.abs(bucketIndex); // Ensure a positive bucket index
+    }
 
-            public Entry(K key, V value) {
-                this.key = key;
-                this.value = value;
-            }
+    private static class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
     }
 }
