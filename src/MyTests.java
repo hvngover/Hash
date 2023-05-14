57c0ed0ac38a527471bf3fc89f2560ba4d4a0e4b
hvngover <hvngover17@gmail.com>
2023-05-15 02:19:49 +0600

    test

--- src/MyTests.java
+++ src/MyTests.java
@@ -7,4 +7,12 @@ class MyTests {
         this.name = name;
     }
 
+    @Override
+    public int hashCode() {
+        int result = 17; // Start with a prime number as the initial result
+        result = 31 * result + id; // Multiply by a prime number and add the id
+        result = 31 * result + (name != null ? name.hashCode() : 0); // Multiply by a prime number and add the hashCode of the name (or 0 if name is null)
+        return result; // Return the final hash code
+    }
+
 }
